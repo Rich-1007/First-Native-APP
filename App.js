532db066654fb197da5e2d9d45e9506e15a87577
Rich-1007 <ghricheak@gmail.com>
2024-04-08 00:26:47 +0530

    Learning core React Native Concepts

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
+import Greet from "./Component/Greet";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 70, backgroundColor: "plum" }}>
+      <Greet name="Rich" />
+      <Greet name="Manoj" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
