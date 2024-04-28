07fadfd5126784756f24b2cfaa140f6986a0f34f
Rich-1007 <ghricheak@gmail.com>
2024-04-28 13:32:55 +0530

    Pressable in Native.

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import CCButton from "./Core-Components/Button";
 import CCImage from "./Core-Components/Image";
+import CCPrassable from "./Core-Components/Prassable";
 import CCScrollView from "./Core-Components/ScrollView";
 import CCText from "./Core-Components/Text";
 import CCView from "./Core-Components/View";
 
 export default function App() {
-  return <CCButton />;
+  return <CCPrassable/>
 }
---
+++ Core-Components/Prassable.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, Image, Pressable, Text, View } from "react-native";
+const logoImg = require("../assets/adaptive-icon.png");
+
+const CCPrassable = () => {
+  return (
+    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
+      <Button
+        title="Press"
+        onPress={() => console.log("Button Pressed!")}
+        color="blue"
+      />
+      <Pressable onPress={() => console.log("Image Pressed")}>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
+
+      <Pressable onPress={() => console.log("Text Pressed")}>
+        <Text>
+          Dear Dipen, I hope this message finds you well. I've sent you a
+          LinkedIn request to connect I'm exploring Automation testing in
+          selenium roles and would appreciate your guidance. Can you give me any
+          advice for finding jobs? Are there any internship for your time and
+          consideration. finding Automation testing jobs? Are there any
+          internship or fresher positions available in your company or through
+          your network? Do you know of any consultancies that arrange interviews
+          for freshers in Hyderabad or Bangalore? Lastly, any essential tips you
+          could share to enhance my chances of landing my first job? Thank you
+          for your time and consideration.
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default CCPrassable;
