47624295e115ce6f5eb07590423098963f0b6d06
Rich-1007 <ghricheak@gmail.com>
2024-04-28 23:02:13 +0530

    Modal in Native.

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+import { Text, View } from "react-native";
 import CCButton from "./Core-Components/Button";
 import CCImage from "./Core-Components/Image";
+import CCModal from "./Core-Components/Modal";
 import CCPrassable from "./Core-Components/Prassable";
 import CCScrollView from "./Core-Components/ScrollView";
 import CCText from "./Core-Components/Text";
 import CCView from "./Core-Components/View";
 
 export default function App() {
-  return <CCPrassable/>
+  return (
+    <CCModal />
+  );
 }
---
+++ Core-Components/Modal.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Button, Modal, Text, View } from "react-native";
+
+const CCModal = () => {
+  const [isModalVariable, setIsModalVariable] = useState(false);
+
+  return (
+    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
+      <Button
+        title="Press"
+        onPress={() => setIsModalVariable(true)}
+        color="midnightblue"
+      />
+
+      <Modal visible={isModalVariable}>
+        <View style={{ backgroundColor: "blue", flex: 1, padding: 60 }}>
+          <Text> Model content</Text>
+
+          <Button
+            title="close"
+            color="midnightblue"
+            onPress={() => setIsModalVariable(false)}
+            // animationType="slide"
+            // presentationStyle="pageSheet"
+          />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default CCModal;
