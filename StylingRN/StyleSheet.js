92622cd10f105068c43c62ed160604d75b9e4640
Rich-1007 <ghricheak@gmail.com>
2024-04-29 12:26:22 +0530

    StyleSheet in Native.

--- StylingRN/StyleSheet.js
+++ StylingRN/StyleSheet.js
@@ -3,15 +3,27 @@ import { Text, View, StyleSheet } from "react-native";
 
 const CCStyleSheet = () => {
   return (
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>StyleSheet API</Text>
+    // </View>
+
     <View style={styles.container}>
-      <Text style={styles.title}>StyleSheet API</Text>
+      <View style={[styles.Box, styles.lightblue]}>
+        <Text style={styles.text}>Light Blue Box</Text>
+      </View>
+      <View style={[styles.Box, styles.lightgreen]}>
+        <Text>Light Green Box</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "pink", padding: 60 },
-  title: { color: "white" },
+  Box: { width: 300, height: 300, padding: 20 },
+  lightblue: { backgroundColor: "lightblue" },
+  lightgreen: { backgroundColor: "lightgreen" },
+  text: { color: "white" },
 });
 
 export default CCStyleSheet;
