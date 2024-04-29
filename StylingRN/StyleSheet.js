9932202de5c31dd17ba104dda5d0dd864112bb50
Rich-1007 <ghricheak@gmail.com>
2024-04-29 13:06:22 +0530

    CSS Inheritance in Native.

--- StylingRN/StyleSheet.js
+++ StylingRN/StyleSheet.js
@@ -8,10 +8,16 @@ const CCStyleSheet = () => {
     // </View>
 
     <View style={styles.container}>
+      <View>
+        <Text style={styles.textwhite}>
+          Style Inheritance <Text style={styles.boldtext}>Text in bold</Text>
+        </Text>
+      </View>
+
       <View style={[styles.Box, styles.lightblue]}>
         <Text style={styles.text}>Light Blue Box</Text>
       </View>
-      <View style={[styles.Box, styles.lightgreen]}>
+      <View style={[styles.Box, styles.lightgreen, styles.androidShadow]}>
         <Text>Light Green Box</Text>
       </View>
     </View>
@@ -20,10 +26,16 @@ const CCStyleSheet = () => {
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "pink", padding: 60 },
-  Box: { width: 300, height: 300, padding: 20 },
+  Box: { width: 300, height: 300, padding: 20, borderRadius: 12, margin: 10 },
   lightblue: { backgroundColor: "lightblue" },
   lightgreen: { backgroundColor: "lightgreen" },
   text: { color: "white" },
+  androidShadow: {
+    elevation: 25,
+  },
+
+  textwhite: { color: "white" },
+  boldtext: { fontWeight: "bold" },
 });
 
 export default CCStyleSheet;
