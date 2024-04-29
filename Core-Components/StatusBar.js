c691e5a6548c7506aa85a07cb7a93a29077339a4
Rich-1007 <ghricheak@gmail.com>
2024-04-29 09:30:20 +0530

    ActivityIndicator in Native.

--- Core-Components/StatusBar.js
+++ Core-Components/StatusBar.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { StatusBar, View } from "react-native";
+import { ActivityIndicator, StatusBar, View } from "react-native";
 
 const CCStatusBar = () => {
   return (
-    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <StatusBar 
-      backgroundColor="yellow" 
-      barStyle="dark-content" 
-      // hidden
-      />
+    <View style={{ backgroundColor: "white", flex: 1, padding: 60 }}>
+      <StatusBar backgroundColor="green" barStyle="dark-content" />
+
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="large" color="midnightblue" />
+      <ActivityIndicator size="large" color="midnightblue" animating={false} />
     </View>
   );
 };
