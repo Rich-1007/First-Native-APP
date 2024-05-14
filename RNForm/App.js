bcbabdbd9d39be5f6048c24afac898e192f13bdd
Rich-1007 <ghricheak@gmail.com>
2024-05-14 17:20:52 +0530

    InputText, Multiline, Switch in Native

--- RNForm/App.js
+++ RNForm/App.js
@@ -1,80 +1,16 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  SafeAreaView,
-  TextInput,
-  Switch,
-} from "react-native";
-import { useState } from "react";
+import React from 'react'
+import FormComponents from './FormComponents'
+import { View } from 'react-native'
 
-export default function App() {
-  const [name, setName] = useState("");
-  const [isDarkMode, setIsDarkMode] = useState(false);
 
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <TextInput
-          style={styles.input}
-          value={name}
-          onChangeText={setName}
-          placeholder="Enter Name"
-          autoCapitalize="none"
-          autoCorrect={false}
-
-          // secureTextEntry
-          // keyboardType="numeric"
-        />
-
-        <TextInput
-          placeholder="Message"
-          style={[styles.input, styles.multiline]}
-          multiline
-        />
-
-        <Text style={styles.text}> My name is {name} </Text>
-
-        <View style={styles.switchcontanner}>
-          <Text style={styles.switchtext}> Dark mode</Text>
-          <Switch
-            value={isDarkMode}
-            onChange={() => setIsDarkMode((prev) => !prev)}
-            trackColor={{ false: "red", true: "blue" }}
-            thumbColor="green"
-          />
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+    <View>
+      <FormComponents />
+      
+      
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: StatusBar.currentHeight,
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    padding: 10,
-    borderWidth: 1,
-  },
-  text: {
-    fontSize: 30,
-    padding: 10,
-  },
-  multiline: {
-    minHeight: 100,
-    textAlignVertical: "top",
-  },
-  switchcontanner: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  switchtext: {
-    fontSize: 32,
-  },
-});
+export default App
---
+++ RNForm/FormComponents.js
@@ -0,0 +1,81 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    StatusBar,
+    SafeAreaView,
+    TextInput,
+    Switch,
+  } from "react-native";
+  import { useState } from "react";
+  
+  export default function FormComponents() {
+    const [name, setName] = useState("");
+    const [isDarkMode, setIsDarkMode] = useState(false);
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <View>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+            placeholder="Enter Name"
+            autoCapitalize="none"
+            autoCorrect={false}
+  
+            // secureTextEntry
+            // keyboardType="numeric"
+          />
+  
+          <TextInput
+            placeholder="Message"
+            style={[styles.input, styles.multiline]}
+            multiline
+          />
+  
+          <Text style={styles.text}> My name is {name} </Text>
+  
+          <View style={styles.switchcontanner}>
+            <Text style={styles.switchtext}> Dark mode</Text>
+            <Switch
+              value={isDarkMode}
+              onChange={() => setIsDarkMode((prev) => !prev)}
+              trackColor={{ false: "red", true: "blue" }}
+              thumbColor="green"
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      paddingTop: StatusBar.currentHeight,
+    },
+    input: {
+      height: 40,
+      margin: 12,
+      padding: 10,
+      borderWidth: 1,
+    },
+    text: {
+      fontSize: 30,
+      padding: 10,
+    },
+    multiline: {
+      minHeight: 100,
+      textAlignVertical: "top",
+    },
+    switchcontanner: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    switchtext: {
+      fontSize: 32,
+    },
+  });
+  
