273956596d9691aa6178eebad7315a52359c79e7
Rich-1007 <ghricheak@gmail.com>
2024-05-17 10:20:12 +0530

    Networking , { Fetch API } , refreshing , onRefresh , Pull on Refresh , { Post Request } , Error Handling  --- In Native.

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -20,35 +20,52 @@ export default function App() {
   const [postbody, setPostbody] = useState("");
 
   const [isposting, setIsposting] = useState(false);
+  const [error, setError] = useState("");
 
   const fetchData = async (limit = 1) => {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
-    );
-    const data = await response.json();
-    setPostlist(data);
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+      );
+      const data = await response.json();
+      setPostlist(data);
+      setIsloading(false);
+      setError("");
+    } catch (error) {
+      setIsloading(false);
+      setError("Failed to fetch post list");
+      console.error("Error adding new post", error);
+    }
   };
 
   const addPost = async () => {
     setIsposting(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
 
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
+          body: JSON.stringify({
+            title: posttitle,
+            body: postbody,
+          }),
+        }
+      );
 
-      body: JSON.stringify({
-        title: posttitle,
-        body: postbody,
-      }),
-    });
-
-    const newPost = await response.json();
-    setPostlist([newPost, ...postlist]);
-    setPosttitle("");
-    setPostbody("");
-    setIsposting(false);
+      const newPost = await response.json();
+      setPostlist([newPost, ...postlist]);
+      setPosttitle("");
+      setPostbody("");
+      setIsposting(false);
+      setError("");
+    } catch (error) {
+      console.error("Error adding new post", error);
+      setError("Failed to add new post");
+    }
   };
 
   useEffect(() => {
@@ -75,48 +92,58 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.postContainer}>
-        <TextInput
-          placeholder="Enter Title"
-          style={styles.postInput}
-          value={posttitle}
-          onChangeText={setPosttitle}
-        />
-        <TextInput
-          placeholder="Enter Body"
-          style={styles.postInput}
-          value={postbody}
-          onChangeText={setPostbody}
-        />
-        <Button
-          title={isposting ? "Adding..." : "Add Post."}
-          disabled={isposting}
-          onPress={addPost}
-        />
-      </View>
-      <View style={styles.listcontainer}>
-        <FlatList
-          data={postlist}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.titletext}>{item.title}</Text>
-                <Text style={styles.bodytext}>{item.body}</Text>
-              </View>
-            );
-          }}
-          ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
-          ListEmptyComponent={
-            <Text style={styles.headerText}>No Post Found</Text>
-          }
-          ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
-          ListFooterComponent={
-            <Text style={styles.footertext}>End of list</Text>
-          }
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-        />
-      </View>
+      {error ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>Error</Text>
+        </View>
+      ) : (
+        <>
+          <View style={styles.postContainer}>
+            <TextInput
+              placeholder="Enter Title"
+              style={styles.postInput}
+              value={posttitle}
+              onChangeText={setPosttitle}
+            />
+            <TextInput
+              placeholder="Enter Body"
+              style={styles.postInput}
+              value={postbody}
+              onChangeText={setPostbody}
+            />
+            <Button
+              title={isposting ? "Adding..." : "Add Post "}
+              disabled={isposting}
+              onPress={addPost}
+            />
+          </View>
+          <View style={styles.listcontainer}>
+            <FlatList
+              data={postlist}
+              renderItem={({ item }) => {
+                return (
+                  <View style={styles.card}>
+                    <Text style={styles.titletext}>{item.title}</Text>
+                    <Text style={styles.bodytext}>{item.body}</Text>
+                  </View>
+                );
+              }}
+              ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+              ListEmptyComponent={
+                <Text style={styles.headerText}>No Post Found</Text>
+              }
+              ListHeaderComponent={
+                <Text style={styles.headerText}>Post List</Text>
+              }
+              ListFooterComponent={
+                <Text style={styles.footertext}>End of list</Text>
+              }
+              refreshing={refreshing}
+              onRefresh={handleRefresh}
+            />
+          </View>
+        </>
+      )}
     </SafeAreaView>
   );
 }
@@ -172,5 +199,25 @@ const styles = StyleSheet.create({
     margin: 16,
     padding: 16,
   },
-  postInput: { fontSize: 16 },
+  postInput: {
+    fontSize: 16,
+    border: 2,
+    padding: 5,
+    
+  },
+  errorContainer: {
+    height: 50,
+    elevation: 30,
+    backgroundColor: "white",
+    marginVertical: 30,
+    marginHorizontal: 50,
+    borderRadius: 8,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorText: {
+    color: "White",
+    fontSize: 30,
+  },
 });
