01d1dc335a993b249b69e4376a56e7c621cc4ed4
Rich-1007 <ghricheak@gmail.com>
2024-05-19 15:19:24 +0530

    useNavigation , route , route.params?.name , navigation , setParams , Update the Data , route.params , navigation.navigate , navigation.setParams , NavigationContainer , Stack.Navigator , Stack.Screen , options-ScreenOptions { headerStyle , headerTitleStyle , headerRight }  ..... In React Native.

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -2,14 +2,53 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
+import { Pressable, Text } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#6a51ae" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+          headerRight: () => (
+            //
+            // <Text>Hello</Text>
+            //
+            <Pressable onPress={() => alert("Menu Button Pressed")}>
+              <Text style={{ color: "#fff", fontSize: 20 }}> Menu </Text>
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: "#e8e",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Welcome Home",
+
+            //
+            // headerStyle: { backgroundColor: "#6a51ae" },
+            // headerTintColor: "#fff",
+            // headerTitleStyle: { fontWeight: "bold" },
+            // headerRight: () => (
+            //   <Pressable onPress={() => alert("Menu Button Pressed")}>
+            //     <Text style={{ color: "#fff", fontSize: 20 }}> Menu </Text>
+            //   </Pressable>
+            // ),
+            // contentStyle: {
+            //   backgroundColor: "#e8e",
+            // },
+            //
+          }}
+        />
         <Stack.Screen
           name="About"
           component={AboutScreen}
